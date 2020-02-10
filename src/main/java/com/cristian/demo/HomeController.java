79eadd90963f06797fda59d6d8a24a34cfb2c259
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-10 16:51:05 -0500

    My Recipe Website

---
+++ src/main/java/com/cristian/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.cristian.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping("/recipe1")
+    public String recipeOne(){
+        return "recipeOne";
+    }
+    @RequestMapping("/recipe2")
+    public String recipeTwo(){
+        return "recipeTwo";
+    }
+    @RequestMapping("/recipe3")
+    public String recipeThree(){
+        return "recipeThree";
+    }
+}
